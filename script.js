08a5adce67579df8e4bee180567570b4e74416be
112233qk <kopetsatrio01@gmail.comgit config --global user.name 112233qkgit config --global user.email kopetsatrio01@gmail.com>
2025-10-05 20:07:52 +0700

    changing description
    from inside profile card to outside and adding animation to profile card and description

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const card = document.querySelector(".profile-card");
+  const extra = document.querySelector(".extra");
   const toggleBtn = document.getElementById("toggle");
   const closeBtn = document.getElementById("close");
 
-  toggleBtn.onclick = () => card.classList.add("activate");
-  closeBtn.onclick = () => card.classList.remove("activate");
+  toggleBtn.onclick = () => {
+    card.classList.add("activate");
+    extra.classList.add("activate");
+  };
+  closeBtn.onclick = () => {
+    card.classList.remove("activate");
+    extra.classList.remove("activate");
+  };
 });
